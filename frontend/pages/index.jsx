91d4fec3610f1683b9a9ebc30ac8b49636dd378f
Brad <bmr023@latech.edu>
2020-02-09 21:07:48 -0600

    Added NGINX reverse proxy to allow for proper backend queries, added material template

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,5 +1,9 @@
+import Dashboard from "./Dashboard.js";
+
+var module = require("./Dashboard.js");
+
 function HomePage() {
     return <div>Welcome to Next.js!</div>
   }
   
-  export default HomePage
+  export default Dashboard
