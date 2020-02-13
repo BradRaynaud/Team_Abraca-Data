fd74ab88658395629dd4b5d65d4720fb262c8360
Brad <bmr023@latech.edu>
2020-02-13 00:28:06 -0600

    Swapped over to bootstrap4 from material UI

--- frontend/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-import Dashboard from "./Dashboard.js";
-
-var module = require("./Dashboard.js");
-
-function HomePage() {
-    return <div>Welcome to Next.js!</div>
-  }
-  
-  export default Dashboard
