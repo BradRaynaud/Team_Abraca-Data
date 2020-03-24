43ae0eacce19e5eb8cc720ba1dea616cae907a6a
DuruFox <dmi57dj@gmail.com>
2020-03-23 22:50:50 -0500

    started empty calls

---
+++ frontend/frontcall.js
@@ -0,0 +1,77 @@
+const unirest = require('unirest');
+
+console.log("Start Tests")
+unirest
+  .get('http://localhost/api/forgot')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .then((response) => {
+    if (response.status == 200)
+    {
+      console.log("Sending You Request")
+    }
+    else
+    {
+      console.log("Request Failed")
+    }
+  })
+
+
+
+  unirest
+  .get('http://localhost/api/signin')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .then((response) => {
+    if (response.status == 200)
+    {
+      console.log("Flask Query: Passed")
+    }
+    else
+    {
+      console.log("Flask Query: Failed")
+    }
+  })
+
+
+  unirest
+  .get('http://localhost/api/signup')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .then((response) => {
+    if (response.status == 200)
+    {
+      console.log("Sent Your Information")
+    }
+    else
+    {
+      console.log("Fill in Information Again")
+    }
+  }) 
+
+
+  unirest
+  .get('http://localhost/api/signup')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .then((response) => {
+    if (response.status == 200)
+    {
+      console.log("Salt & Hash: Passed")
+    }
+    else
+    {
+      console.log("Salt & Hash: Failed")
+    }
+  })
+
+
+  unirest
+  .get('http://localhost/api/login')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .then((response) => {
+    if (response.status == 200)
+    {
+      console.log("Login Verify: Passed")
+    }
+    else
+    {
+      console.log("Login Verify: Failed")
+    }
+  }) 
---
+++ frontend/frontindex.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const unirest = require('unirest');
+const bcrypt = require('bcrypt');
+var mysql = require('mysql');
+
+// Connection URL
+const url = 'mongodb://root:example@mongo:27017';
+ 
+// Database Name
+const dbName = 'foodthings';
+
+
+var options = {
+    user: "admin",
+    pass: "pass"
+    };
+
+// Create express app and add middleware
+const app = express();
+app.use(express.json());
+
+ 
+app.get("/forgot", function(req, res) {
+    MongoClient.connect(url, function(err, client) {
+        console.log("Connected successfully to server");
+
+        const db = client.db(dbName);
+        console.log("DB found");
+
+        var myobj = {title: "Grilled Chicken", difficulty: "chicken", time: "chicken", ingredients: {1: "chicken", 2: "olive oil", 3: "seasoning"}, steps: {1: "Place chicken on pan", 2: "Simmer chicken on medium heat", 3: "Put chicken on plate when done cooking"}};
+
+        db.collection("recipes").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 recipe inserted");
+        });
+
+        client.close();
+        console.log("Connection closed");
+    });
+})
+
+
+
+app.get('/forgot', function (req, res) {
+    console.log("Call recieved")
+    unirest
+    .post('http://datamining:5000/HelloWorld')
+    .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+    .send({ "parameter": 23, "foo": "bar" })
+    .then((response) => {
+        console.log(response.body)
+        res.status(200).json({"message": response.body});
+  })
+})
+
+
+
+
+
+
+
+
+
