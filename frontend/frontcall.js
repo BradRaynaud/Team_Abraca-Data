1f4325e6f168fea278fc77fe4c9bc4595c2a6975
DuruFox <dmi57dj@gmail.com>
2020-03-23 23:23:07 -0500

    finished empty api calls when form actions are done, and started modifying our template web pages for actual functionality

--- frontend/frontcall.js
+++ frontend/frontcall.js
@@ -2,7 +2,7 @@ const unirest = require('unirest');
 
 console.log("Start Tests")
 unirest
-  .get('http://localhost/api/forgot')
+  .post('http://localhost/api/forgot')
   .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
   .then((response) => {
     if (response.status == 200)
@@ -18,22 +18,22 @@ unirest
 
 
   unirest
-  .get('http://localhost/api/signin')
+  .post('http://localhost/api/signin')
   .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
   .then((response) => {
     if (response.status == 200)
     {
-      console.log("Flask Query: Passed")
+      console.log("Successfully signed in")
     }
     else
     {
-      console.log("Flask Query: Failed")
+      console.log("Please Check Username\Password")
     }
   })
 
 
   unirest
-  .get('http://localhost/api/signup')
+  .post('http://localhost/api/signup')
   .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
   .then((response) => {
     if (response.status == 200)
--- frontend/frontindex.js
+++ frontend/frontindex.js
@@ -21,18 +21,18 @@ const app = express();
 app.use(express.json());
 
  
-app.get("/forgot", function(req, res) {
+app.get("/signin", function(req, res) {
     MongoClient.connect(url, function(err, client) {
         console.log("Connected successfully to server");
 
         const db = client.db(dbName);
-        console.log("DB found");
+        console.log("Verifying Authentication...");
 
-        var myobj = {title: "Grilled Chicken", difficulty: "chicken", time: "chicken", ingredients: {1: "chicken", 2: "olive oil", 3: "seasoning"}, steps: {1: "Place chicken on pan", 2: "Simmer chicken on medium heat", 3: "Put chicken on plate when done cooking"}};
+        var myobj = {Username:"Simp-R-us", Password:"BigSImpBOI2005"};
 
-        db.collection("recipes").insertOne(myobj, function(err, res) {
+        db.collection("Client").insertOne(myobj, function(err, res) {
             if (err) throw err;
-            console.log("1 recipe inserted");
+            console.log("Sign in successful");
         });
 
         client.close();
@@ -42,10 +42,10 @@ app.get("/forgot", function(req, res) {
 
 
 
-app.get('/forgot', function (req, res) {
+app.get('/signup', function (req, res) {
     console.log("Call recieved")
     unirest
-    .post('http://datamining:5000/HelloWorld')
+    .post('http://Clientdata:4000/Client')
     .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
     .send({ "parameter": 23, "foo": "bar" })
     .then((response) => {
@@ -55,7 +55,7 @@ app.get('/forgot', function (req, res) {
 })
 
 
-app.get("/mongoroute", function(req,res){
+app.get("/forgot", function(req,res){
     // Use connect method to connect to the server
     MongoClient.connect(url, function(err, client) {
         console.log("Connected successfully to server");
@@ -63,7 +63,7 @@ app.get("/mongoroute", function(req,res){
         const db = client.db(dbName);
         console.log("DB found");        
         
-        db.collection("recipes").find({}).toArray(function(error, docs)
+        db.collection("email").find({}).toArray(function(error, docs)
         {
             if (error) throw error;
             console.log("Found the following records");
